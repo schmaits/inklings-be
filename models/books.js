67e7d5d423dd39ae34c3f1c5b917a082b7417efd
schmaits <stansfield.c.r@gmail.com>
2018-04-17 12:33:05 +0100

    Adds required fields

--- models/books.js
+++ models/books.js
@@ -2,18 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const BooksSchema = new Schema({
-    title: String,
-    author: String,
-    summary: String,
-    year: Number,
-    coverImageUrl: String,
-    genres: [String],
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    coverImageUrl: {
+        type: String,
+        required: true
+    },
+    genres: {
+        type: [String],
+        required: true
+    },
     rating: {
         type: Number, 
         min: 0,
-        max: 5
+        max: 5,
+        default: 0
     },
-    country: String
+    country: {
+        type: String,
+        required: true
+    }
 });
 
 module.exports = BooksSchema;
