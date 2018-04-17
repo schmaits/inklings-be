1012e5da7f684b5cefc3fbc44eb2a2fa06a533b6
schmaits <stansfield.c.r@gmail.com>
2018-04-17 12:23:51 +0100

    Creates books schema

---
+++ models/books.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BooksSchema = new Schema({
+    title: String,
+    author: String,
+    summary: String,
+    year: Number,
+    coverImageUrl: String,
+    genres: [String],
+    rating: {
+        type: Number, 
+        min: 0,
+        max: 5
+    },
+    country: String
+});
