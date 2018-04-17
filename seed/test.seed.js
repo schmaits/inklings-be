5edd8cae6efc0ede8c54186a4264502d6f18edf9
schmaits <stansfield.c.r@gmail.com>
2018-04-17 15:17:57 +0100

    Saves quotes

--- seed/test.seed.js
+++ seed/test.seed.js
@@ -54,6 +54,52 @@ const saveBooks = () => {
     return Promise.all(books);
 };
 
+const saveQuotes = () => {
+    const quotes = [
+        {
+            body: 'Already my childhood seemed far away—a remote age, faded and bittersweet, like dried flowers. Did I regret its loss, did I want it back? I didn\'t think so.',
+            book: savedData.books[2]._id
+        },
+        {
+            body: 'One look at a banana and you can tell it came from outer space.',
+            book: savedData.books[2]._id
+        },
+        {
+            body: 'Stick a shovel in the ground almost anywhere and some horrible thing or other will come to light.',
+            book: savedData.books[2]._id
+        },
+        {
+            body: 'There ain\'t no sin and there ain\'t no virtue. There\'s just stuff people do.',
+            book: savedData.books[0]._id
+        },
+        {
+            body: 'It was her habit to build up laughter out of inadequate materials.',
+            book: savedData.books[0]._id
+        },
+        {
+            body: 'And the little screaming fact that sounds through all history: repression works only to strengthen and knit the repressed.',
+            book: savedData.books[0]._id
+        },
+        {
+            body: 'Memories, even your most precious ones, fade surprisingly quickly. But I don’t go along with that. The memories I value most, I don’t ever see them fading.',
+            book: savedData.books[1]._id
+        },
+        {
+            body: 'Sometimes I get so immersed in my own company, if I unexpectedly run into someone I know, it\'s a bit of a shock and takes me a while to adjust.',
+            book: savedData.books[1]._id
+        },
+        {
+            body: 'She always wanted to believe in things.',
+            book: savedData.books[1]._id
+        }
+    ].map(quote => {
+        quote = new Quotes(quote);
+        savedData.quotes.push(quote);
+        quote.save();
+    });
+    return Promise.all(quotes);
+};
+
 mongoose.connect(DB)
     .then(() => {
         console.log(`Successfully connected to ${DB}`);
@@ -67,8 +113,11 @@ mongoose.connect(DB)
     })
     .then(() => {
         console.log(`Saved ${savedData.books.length} books`);
+        return saveQuotes();
+    })
+    .then(() => {
+        console.log(`Saved ${savedData.quotes.length} quotes`);
     })
     .catch(err => {
         console.log(err);
     });
-    
