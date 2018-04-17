3fe0ff2c6353fe890a22a813a6cf72e44af9b49c
schmaits <stansfield.c.r@gmail.com>
2018-04-17 13:00:06 +0100

    Creates clubs schema

---
+++ models/clubs.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+
+const ClubsSchema = new Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String
+    },
+    members: [Schema.Types.ObjectId],
+    currentlyReading: Schema.Types.ObjectId,
+    read: [Schema.Types.ObjectId]
+});
+
+module.exports = ClubsSchema;
