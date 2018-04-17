9b983a92e62402061fb92aeb80469e8996253c79
schmaits <stansfield.c.r@gmail.com>
2018-04-17 12:35:39 +0100

    Creates quotes schema

---
+++ models/quotes.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+
+const QuotesSchema = new Schema({
+    book: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = QuotesSchema;
