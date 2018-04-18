c48d553e266949ab3625b33ecac8bc408fb8cd4a
schmaits <stansfield.c.r@gmail.com>
2018-04-18 12:33:20 +0100

    amended so database re-seeds before every test, tests for POST /api/clubs/ endpoint - passing

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,11 +1,26 @@
 process.env.NODE_ENV = 'test';
 
+const mongoose = require('mongoose');
 const { expect } = require('chai');
 const request = require('supertest');
 const app = require('../server');
+const seedTestDatabase = require('../seed/test.seed');
 
-describe('API endpoints', () => {
+describe('API endpoints', () => {    
+    beforeEach((done) => {
+        seedTestDatabase()
+            .then(() => {
+                done();
+            });
+    });
+    
+    afterEach(() => {
+        mongoose.connection.close();
+        console.log('Connection closed');
+    });
+    
     describe('GET /api/clubs', () => {
+
         it('should return an array of all clubs', (done) => {
             request(app)
                 .get('/api/clubs')
@@ -15,5 +30,23 @@ describe('API endpoints', () => {
                     done();
                 });
         });
+
+    });
+
+    describe('POST /api/clubs', () => {
+        it('should add a new club', (done) => {
+            request(app)
+                .post('/api/clubs')
+                .send({
+                    name: 'test club'
+                })
+                .end((err, res) => {
+                    if (err) throw err;
+                    expect(res.status).to.equal(201);
+                    expect(res.body.newClub).to.have.property('_id');
+                    done();
+                });
+        });
     });
 });
+
