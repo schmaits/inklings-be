a3b57e9b773c2541c98c72954a8330a5e999c06e
schmaits <stansfield.c.r@gmail.com>
2018-04-18 13:23:57 +0100

    adds error handling for if an unknown ID is requested

--- controllers/clubs.controllers.js
+++ controllers/clubs.controllers.js
@@ -29,7 +29,8 @@ module.exports = {
                 res.status(200).json({club});
             })
             .catch(err => {
-                res.status(404).send(err);
+                if (err.message === 'That ID does not exist') res.status(404).send(err);
+                res.status(500).send(err);
             });
     }
 };
