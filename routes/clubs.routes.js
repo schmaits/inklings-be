8ce5a968e250e7ba72d701dcac676db92def0ebc
schmaits <stansfield.c.r@gmail.com>
2018-04-18 15:30:20 +0100

    Creates route for PUT /api/clubs/:clubId/users endpoint

--- routes/clubs.routes.js
+++ routes/clubs.routes.js
@@ -1,10 +1,12 @@
 const clubsRouter = require('express').Router();
-const { getAllClubs, addNewClub, getOneClub } = require('../controllers/clubs.controllers');
+const { getAllClubs, addNewClub, getOneClub, updateMembersList } = require('../controllers/clubs.controllers');
 
 clubsRouter.get('/', getAllClubs);
 
 clubsRouter.get('/:clubId', getOneClub);
 
 clubsRouter.post('/', addNewClub);
+
+clubsRouter.put('/:clubId/users', updateMembersList);
 
 module.exports = clubsRouter;
