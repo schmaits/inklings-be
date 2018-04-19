da946b540cad08750456698fcc8d0afd286e138f
schmaits <stansfield.c.r@gmail.com>
2018-04-19 13:57:22 +0100

    Creates route for /users

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,9 +1,12 @@
 const apiRouter = require('express').Router();
 const clubsRouter = require('./clubs.routes');
 const booksRouter = require('./books.routes');
+const usersRouter = require('./users.routes');
 
 apiRouter.use('/clubs', clubsRouter);
 
 apiRouter.use('/books', booksRouter);
+
+apiRouter.use('/users', usersRouter);
 
 module.exports = apiRouter;
