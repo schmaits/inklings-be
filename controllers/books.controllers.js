b819c88f86d572a338e2b0e84bd5f6d8ad71bcb9
schmaits <stansfield.c.r@gmail.com>
2018-04-19 11:53:34 +0100

    Creates controller for GET /api/books/:bookId endpoint

--- controllers/books.controllers.js
+++ controllers/books.controllers.js
@@ -1,10 +1,21 @@
-const { getAllBooks } = require('../lib/books.queries.js');
+const { getAllBooks, getOneBook } = require('../lib/books.queries.js');
 
 module.exports = {
     getAllBooks: (req, res) => {
         getAllBooks()
             .then(allBooks => {
                 res.status(200).json({allBooks});
+            })
+            .catch(err => {
+                res.status(500).send(err);
+            });
+    },
+
+    getOneBook: (req, res) => {
+        const bookId = req.params.bookId;
+        getOneBook(bookId)
+            .then(book => {
+                res.status(200).json({book});
             })
             .catch(err => {
                 res.status(500).send(err);
