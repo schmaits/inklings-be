ccbce3f8a41ffd85180e68c5cae0869c2a82f6a2
schmaits <stansfield.c.r@gmail.com>
2018-04-19 09:25:32 +0100

    Creates route for /books

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,6 +1,9 @@
 const apiRouter = require('express').Router();
 const clubsRouter = require('./clubs.routes');
+const booksRouter = require('./books.routes');
 
 apiRouter.use('/clubs', clubsRouter);
+
+apiRouter.use('/books', booksRouter);
 
 module.exports = apiRouter;
