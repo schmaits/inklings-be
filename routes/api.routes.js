3312d6889f106db67984f1501117a8f54c3c7e9f
schmaits <stansfield.c.r@gmail.com>
2018-04-20 11:39:51 +0100

    Adds route for /api/queries

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -3,6 +3,7 @@ const clubsRouter = require('./clubs.routes');
 const booksRouter = require('./books.routes');
 const usersRouter = require('./users.routes');
 const commentsRouter = require('./comments.routes');
+const quotesRouter = require('./quotes.routes');
 
 apiRouter.use('/clubs', clubsRouter);
 
@@ -11,5 +12,7 @@ apiRouter.use('/books', booksRouter);
 apiRouter.use('/users', usersRouter);
 
 apiRouter.use('/comments', commentsRouter);
+
+apiRouter.use('/quotes', quotesRouter);
 
 module.exports = apiRouter;
