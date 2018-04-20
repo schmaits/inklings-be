be80ca06efbc00f4ea7fbf2052cb439a9a962000
schmaits <stansfield.c.r@gmail.com>
2018-04-20 15:58:57 +0100

    Saves quotes

--- seed/dev.seed.js
+++ seed/dev.seed.js
@@ -4,25 +4,38 @@ const faker = require('faker');
 const DB = 'mongodb://localhost/book-club-api';
 const { Books, Clubs, Comments, Quotes, Users } = require('../models/models');
 const booksData = require('./data/books.json');
+const quotesData = require('./data/quotes.json');
 
 mongoose.Promise = Promise;
 
 const savedData = {};
 
 const saveBooks = () => {
-    const books = booksData.books.map(book => {
+    let books = booksData.books.map(book => {
         return {
             title: book.title,
             author: book.author,
             year: book.year,
             coverImageUrl: book.imageLink,
             genres: book.genres,
-            rating: Math.round(Math.random()) * 5,
+            rating: Math.round(Math.random() * 5),
             country: book.country
         };
-    });
-    books.map(book => new Books(book).save());
-    return Promise.all(booksData.books);
+    }).map(book => new Books(book).save());
+    return Promise.all(books);
+};
+
+const saveQuotes = () => {
+    const quotes = quotesData.quotes.map(quote => {
+        let book = savedData.books.filter(book => {
+            return book.title === quote.book;
+        });
+        return {
+        body: quote.body,
+        book: book[0]._id
+        };
+    }).map(quote => new Quotes(quote).save());
+    return Promise.all(quotes);
 };
 
 mongoose.connect(DB)
@@ -37,4 +50,13 @@ mongoose.connect(DB)
     .then(savedBooks => {
         console.log(`Saved ${savedBooks.length} books`);
         savedData.books = savedBooks;
+        return saveQuotes();
+    })
+    .then(savedQuotes => {
+        console.log(`Saved ${savedQuotes.length} quotes`);
+        savedData.quotes = savedQuotes;
+    })
+    .catch(err => {
+        console.log(err);
     });
+
